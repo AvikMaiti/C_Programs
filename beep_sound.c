ff3f3d8af848eaa2a428482cd8e0153147c0a3bc
Avik <82557491+AvikMaiti@users.noreply.github.com>
2023-09-11 12:17:49 +0530

    added new file

---
+++ beep_sound.c
@@ -0,0 +1,11 @@
+//  a program that produce beep sound (behavior might vary depending on the OS)
+
+// Header file
+#include<stdio.h>
+
+// Main function
+int main()
+{
+    printf("\a"); // It produces a beep sound
+    return 0;
+}
