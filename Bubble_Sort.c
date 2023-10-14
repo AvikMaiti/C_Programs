7e82021669c9de81d817611500d74bc8d76b6548
Avik Maiti <avikmaiti.in@gmail.com>
2023-10-14 22:50:30 +0530

    algorithm added

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -1,7 +1,68 @@
+// Algorithm
+//
+// bubble_sort(array[n])
+//      if n == 0, return
+//      for all the element of the array upto n
+//      if array[i]>array[i+1]
+//          swap two element
+//      bubble_sort(array[n-1])
+//
+// -------------------------------------------------------------------------
 // Bubble Sort using recursion
-
-// Insertion of an element in an array
 #include <stdio.h>
+
+// function diclaration
+void read_elements();
+void bubble_sort();
+void check_element();
+void swap();
+void display_elements();
+void display_elements_descending();
+
+// golbal variable
+int no, arr[100], chk_upto, i, idx;
+
+// main function
+int main(void)
+{
+    read_elements();
+    bubble_sort();
+    display_elements();
+    display_elements_descending();
+}
+
+// read array elements from user
+void read_elements()
+{
+    printf("Enter no of element:\t");
+    scanf("%d", &no);
+    chk_upto = no;
+    printf("Enter elements:\t");
+    for (i = 0; i < no; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+}
+
+// main function for inserton sort
+void bubble_sort()
+{
+    if (chk_upto == 0)
+        return;
+    check_element(0);
+    chk_upto--;
+    bubble_sort();
+}
+
+// find largest element by compairing every element
+void check_element(int idx)
+{
+    if (idx == chk_upto - 1)
+        return;
+    if (arr[idx] > arr[idx + 1])
+        swap(&arr[idx], &arr[idx + 1]);
+    check_element(idx + 1);
+}
 
 // Swap two numbers
 void swap(int *a, int *b)
@@ -11,43 +72,25 @@ void swap(int *a, int *b)
     *b = store;
 }
 
-// find largest element by compairing every element
-void check_element(int a[], int size, int idx)
+// for displaying array (Ascending order)
+void display_elements()
 {
-    if (idx == size - 1)
-        return;
-    if (a[idx] > a[idx + 1])
-        swap(&a[idx], &a[idx + 1]);
-    check_element(a, size, idx + 1);
-}
-
-// user defined functon for bubble sort
-void bubble(int a[], int length)
-{
-    if (length <= 1)
-        return;
-    check_element(a, length, 0);
-    bubble(a, length - 1);
-}
-
-// main function
-int main(void)
-{
-    int i, n;
-    printf("Enter no of elements in array: ");
-    scanf("%d", &n);
-    int arr[n];
-    // takaing array elements from user
-    printf("Enter elements of the array:\n");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    bubble(arr, n);
-    // printing sorted array
-    printf("Array elements after sort using Bubble Sort:\n");
-    for (i = 0; i < n; i++)
+    printf("Array elements after sorted using Bubble Sort:\n");
+    printf("Ascending order:\t");
+    for (i = 0; i < no; i++)
     {
         printf("%d ", arr[i]);
     }
+    printf("\n\n");
+}
+
+// for displaying array (Decending order)
+void display_elements_descending()
+{
+    printf("Descending order:\t");
+    for (i = no - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
 }
--- Insertion_Sort.c
+++ Insertion_Sort.c
@@ -1,9 +1,23 @@
+// Algorithm
+//
+// 1. consider the first element to be sorted sub-array and rest all the elements are unsorted sub-array
+// 2. pick next element and store in a variable and compare with the previous elements (Sorted Array) with the current value
+// 3. until the sorted sub-array elements larger than the current value, shift the elements by one and repeat step 3
+// 4. insert the value
+// 5. repeat step 2, until list is sorted
+// 6. Exit
+//
+// -------------------------------------------------------------------------------
+
+// Insertion Sort using recursion
 #include <stdio.h>
+
 // function diclaration
 void read_elements();
 void insertion_sort();
 void display_elements();
-void compare();
+void display_elements_descending();
+
 // golbal variable
 int no, arr[100], i, partition = 1, idx, temp;
 
@@ -13,6 +27,7 @@ int main(void)
     read_elements();
     insertion_sort();
     display_elements();
+    display_elements_descending();
 }
 
 // read array elements from user
@@ -20,7 +35,7 @@ void read_elements()
 {
     printf("Enter no of element:\t");
     scanf("%d", &no);
-    printf("Enter element:\t");
+    printf("Enter elements:\t");
     for (i = 0; i < no; i++)
     {
         scanf("%d", &arr[i]);
@@ -32,14 +47,6 @@ void insertion_sort()
 {
     if (partition == no || no == 1)
         return;
-    compare();
-}
-
-// for comparing elements and sort one by one
-void compare()
-{
-    if (partition >= no)
-        return;
     temp = arr[partition];
     idx = partition - 1;
     while (idx >= 0 && arr[idx] > temp)
@@ -49,14 +56,26 @@ void compare()
     }
     arr[idx + 1] = temp;
     partition++;
-    compare();
+    insertion_sort();
 }
 
-// for displaying array
+// for displaying array (Ascending order)
 void display_elements()
 {
-    printf("Array elements after sorted using Insertion Sort:\n\n");
+    printf("Array elements after sorted using Insertion Sort:\n");
+    printf("Ascending order:\t");
     for (i = 0; i < no; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
+}
+
+// for displaying array (Decending order)
+void display_elements_descending()
+{
+    printf("Descending order:\t");
+    for (i = no - 1; i >= 0; i--)
     {
         printf("%d ", arr[i]);
     }
