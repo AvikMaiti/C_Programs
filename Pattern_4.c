13ad5cd77491adc62a24864f0f9bb2a95c9a305b
Avik Maiti <avikmaiti.in@gmail.com>
2023-10-25 12:18:41 +0530

    new program

---
+++ Pattern_4.c
@@ -0,0 +1,26 @@
+/*  rectangle star pattern
+
+ * * * * * * * * * * *
+ * * * * * * * * * * *
+ * * * * * * * * * * *
+ * * * * * * * * * * *          */
+
+#include <stdio.h>
+int main(void)
+{
+    int h, w, i, j;
+    printf("Enter height: ");
+    scanf("%d", &h);
+    printf("Enter width: ");
+    scanf("%d", &w);
+    printf("\n");
+    for (i = 0; i < h; i++)
+    {
+        for (j = 0; j < w; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
